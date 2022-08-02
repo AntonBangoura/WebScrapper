cbd6d7bac7246813d670158a14141e5d41bec994
Anton Bangoura <bangouranton@gmail.com>
2022-08-02 14:02:30 +0200

    final

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Le serveur est actif au port ${PORT}`));
+
+const url = 'https://www.theguardian.com/uk'
+
+axios(url).then(response => {
+    const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.fc-item__title', html).each(function(){
+           const title = $(this).text()
+           const url = $(this).find('a').attr('href')
+           articles.push({
+            title,
+            url
+           })
+        })
+        console.log(articles)
+}).catch(err => console.log(err => console.log(err)))
